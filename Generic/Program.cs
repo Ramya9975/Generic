3591d66561f3ea16f03bf9bc5f3914edab943334
HAI <HAI@DESKTOP-R7PD6US>
2023-02-14 18:13:25 +0530

    Add project files.

---
+++ Generic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Equal = calculator.AreEqual("ab", "ab");
+
+            if (Equal)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+            }
+            
+
+        }
+    }
+}
+public class calculator
+{
+    public static bool AreEqual<T>( T value1, T value2)
+    {
+        return value1.Equals(value2);
+    }
+   
+}
+
